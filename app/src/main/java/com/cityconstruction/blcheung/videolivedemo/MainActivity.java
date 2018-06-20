729e0f0bd2855e75988913667e8d970256116925
BLCheung <925306022@qq.com>
2018-06-20 20:32:15 +0800

    First commit.

---
+++ app/src/main/java/com/cityconstruction/blcheung/videolivedemo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.cityconstruction.blcheung.videolivedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.cityconstruction.blcheung.videolivedemo.adapter.ItemSelectorAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+
+    private RecyclerView recyclerView;
+    private List<String> programNameList = new ArrayList<>();
+    private ItemSelectorAdapter adapter;
+    private String[] programName = {
+            "CCTV-1综合", "CCTV-2财经", "CCTV-3综艺", "CCTV-4中文国际", "CCTV-5体育", "CCTV-6电影",
+            "CCTV-7军事农业", "CCTV-8电视剧", "CCTV-9纪录片", "CCTV-10科教", "CCTV-11戏曲",
+            "CCTV-12社会与法", "CCTV-13新闻", "CCTV-14少儿", "CCTV-15音乐"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initProgram();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayout.VERTICAL, false));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(new ItemSelectorAdapter(this, programNameList));
+    }
+
+    private void initView() {
+        recyclerView = findViewById(R.id.recyclerView);
+    }
+
+
+    private void initProgram() {
+        for (int i = 0; i < programName.length; i++) {
+            programNameList.add(programName[i]);
+            Log.d(TAG, "initProgram: " + i);
+        }
+    }
+}
---
+++ app/src/main/java/com/cityconstruction/blcheung/videolivedemo/adapter/ItemSelectorAdapter.java
@@ -0,0 +1,75 @@
+package com.cityconstruction.blcheung.videolivedemo.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.cityconstruction.blcheung.videolivedemo.R;
+
+import java.util.List;
+
+/**
+ * Created by BLCheung.
+ * Date:2018/6/20 0:07
+ */
+public class ItemSelectorAdapter extends RecyclerView.Adapter<ItemSelectorAdapter.ViewHolder> {
+    private String TAG = "ItemSelectorAdapter";
+    private Context context;
+    private List<String> programNameList;
+
+    private String[] programUrlList = new String[]{
+            "http://223.110.243.138/PLTV/2510088/224/3221227177/index.m3u8",
+            "http://223.110.243.136/PLTV/2510088/224/3221227199/index.m3u8",
+            "http://223.110.243.140/PLTV/2510088/224/3221227165/index.m3u8",
+            "http://223.110.243.138/PLTV/2510088/224/3221227162/index.m3u8",
+            "http://223.110.243.140/PLTV/2510088/224/3221227202/index.m3u8",
+            "http://223.110.243.140/PLTV/2510088/224/3221227145/index.m3u8",
+            "http://223.110.243.136/PLTV/2510088/224/3221227142/index.m3u8",
+            "http://223.110.245.167/ott.js.chinamobile.com/PLTV/3/224/3221227204/index.m3u8",
+            "http://223.110.243.140/PLTV/2510088/224/3221227156/index.m3u8",
+            "http://223.110.243.140/PLTV/2510088/224/3221227161/index.m3u8",
+            "http://223.110.243.140/PLTV/2510088/224/3221227174/index.m3u8",
+            "http://223.110.243.140/PLTV/2510088/224/3221227171/index.m3u8",
+            "http://dbiptv.sn.chinamobile.com/PLTV/88888890/224/3221225508/index.m3u8",
+            "http://223.110.243.140/PLTV/2510088/224/3221227247/index.m3u8",
+            "http://183.207.249.7/PLTV/3/224/3221225568/index.m3u8"
+    };
+
+    public ItemSelectorAdapter(Context context, List<String> programNameList) {
+        this.context = context;
+        this.programNameList = programNameList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.program_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String s = programNameList.get(position);
+        Log.d(TAG, "onBindViewHolder: " + s);
+        holder.tvProgramItem.setText(s);
+    }
+
+    @Override
+    public int getItemCount() {
+        return programNameList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvProgramItem;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvProgramItem = itemView.findViewById(R.id.tv_program_item);
+        }
+    }
+}
